b68dbdb66171e7aefedb65fce64dd94a3f65b82b
elmanoneto <elmanoneto@gmail.com>
2022-04-28 17:51:12 -0300

    Push site

---
+++ main.js
@@ -0,0 +1,23 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <h1>Hey!</h1>
+  <p>
+    My name's Elmano, I'm a Software Engineer.
+    Currently, I'm working at <a href="http://www.liferay.com" target="_blank">Liferay</a>.
+    I like to work and study things around front-end world.
+  </p>
+
+  <ul>
+    <li>
+      <a href="http://www.facebook.com/elmanoneto" class="social-icon" target="_blank"><i class="fa fa-facebook"></i></a>
+    </li>
+    <li>
+      <a href="http://www.instagram.com/elmanoneto" class="social-icon" target="_blank"><i class="fa fa-instagram"></i></a>
+    </li>
+    <li>
+      <a href="http://www.github.com/elmanoneto" class="social-icon" target="_blank"><i class="fa fa-github"></i></a>
+    </li>
+  </ul>
+
+`
